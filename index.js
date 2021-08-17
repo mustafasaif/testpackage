781bb99308abdf0ab410d6f352485c7779a872f0
Mustafa <mustafa.s@beem.africa>
2021-08-17 16:30:17 +0300

    updated error handling

--- index.js
+++ index.js
@@ -9,30 +9,18 @@ uploadfile = async (filename) => {
   try {
     if (filename) {
       fileTypeCheckResults = checkFileType(filename);
-      console.log({ fileTypeCheckResults });
       if (fileTypeCheckResults) {
         fileHeadersCheckResults = await checkFileHeaders(filename);
-        console.log({ fileHeadersCheckResults });
       } else {
-        console.log("Invalid File Extension");
+        console.log("Error => Invalid File Extension");
       }
       if (fileHeadersCheckResults) {
-        //  const contacts = await fileContent(filename);
-        //  //console.log(contacts);
-        //   const gets = await getDuplicateContacts(contacts);
-        //   const gets4 = await removeDuplicateContacts(contacts,gets);
-        //   console.log(gets4.length);
-        // //  const gets2 = await getRemoveSpace(contacts);
-        // //  const gets3 = await getValidContacts(contacts);
-        //   console.log(gets.length);
-        // //  console.log(gets3.length);
-        // // // console.log(contacts);
         return true;
       } else {
-        console.log("Invalid Header Names");
+        console.log("Error => Invalid Header Names");
       }
     } else {
-      console.log("No File Path Defined");
+      console.log("Error => No File Path Defined");
     }
   } catch (error) {
     console.log(error);
